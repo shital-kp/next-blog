dc79da87efe71ff04e4e5fc49cb212f94a71bf14
Shital Ghatole <shital.ghatole@kraftpixel.com>
2024-09-11 17:06:34 +0530

    add social logos and links in header.tsx

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,9 +1,27 @@
+import Image from "next/image"
+import Link from "next/link"
 
 
-const Navbar = () => {
+const Header = () => {
   return (
-    <div className="">Navbar</div>
+    <div className="">
+      <div className="">
+        <Image src="/facebook.svg" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.svg" alt="instagram" width={24} height={24} />
+        <Image src="/youtube.svg" alt="youtube" width={24} height={24} />
+      </div>
+
+      <div className="">
+        NextBlog
+      </div>
+
+      <div className="">
+        <Link href="/">Homepage</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Header
